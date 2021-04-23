854137637b26135c784d49f6d137eec0a87bd73f
Toby <60394163+tobywalls@users.noreply.github.com>
2021-04-23 10:24:48 +0100

    Add pyspark code, s3 bucket object

---
+++ emr_steps.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket_object" "adg_incremental_spark_step" {
+  bucket = data.terraform_remote_state.common.outputs.config_bucket["id"]
+  key    = "${local.replica_emr_step_scripts_s3_prefix}/spark-job.py"
+  content = templatefile("files/emr/spark-job.py",
+  {
+    dks_decrypt_endpoint = data.terraform_remote_state.crypto.outputs.dks_endpoint[local.environment]
+    log_path = "/var/log/adg_incremental_step.log"
+    # todo - temporary output location
+    incremental_output_bucket = data.terraform_remote_state.ingest.outputs.s3_buckets["input_bucket"]
+    incremental_output_prefix = "business-data/intra-day/"
+  })
+
+  tags = merge(
+  local.common_tags,
+  {
+    Name = "adg-incremental-spark-step"
+  },
+  )
+}
---
+++ files/emr/spark-job.py
@@ -0,0 +1,242 @@
+#!/usr/bin/python3
+import argparse
+import base64
+import binascii
+import datetime
+import itertools
+import json
+import os
+import time
+from subprocess import PIPE, run
+import logging
+import sys
+import re
+
+import boto3
+import requests
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+from pyspark.sql import SparkSession
+from pyspark.sql.types import Row
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3 import Retry
+import concurrent.futures
+
+DKS_ENDPOINT = "${dks_decrypt_endpoint}"
+DKS_DECRYPT_ENDPOINT = DKS_ENDPOINT + "/datakey/actions/decrypt/"
+
+INCREMENTAL_OUTPUT_BUCKET = "${incremental_output_bucket}"
+INCREMENTAL_OUTPUT_PREFIX = "${incremental_output_prefix}"
+
+
+def setup_logging(log_level, log_path):
+    logger = logging.getLogger()
+    for old_handler in logger.handlers:
+        logger.removeHandler(old_handler)
+
+    if log_path is None:
+        handler = logging.StreamHandler(sys.stdout)
+    else:
+        handler = logging.FileHandler(log_path)
+
+    json_format = "{ 'timestamp': '%(asctime)s', 'log_level': '%(levelname)s', 'message': '%(message)s' }"
+    handler.setFormatter(logging.Formatter(json_format))
+    logger.addHandler(handler)
+    new_level = logging.getLevelName(log_level.upper())
+    logger.setLevel(new_level)
+
+    return logger
+
+
+_logger = setup_logging(
+    log_level="INFO",
+    log_path="${log_path}",
+)
+
+
+def get_parameters():
+    """Define and parse command line args."""
+    parser = argparse.ArgumentParser(
+        description="Receive args provided to spark submit job"
+    )
+    # Parse command line inputs and set defaults
+    parser.add_argument("--correlation_id", default=0, type=int)
+    parser.add_argument("--start_time", default=0, type=int)
+    parser.add_argument(
+        "--end_time",
+        default=int(datetime.datetime.now(datetime.timezone.utc).timestamp() * 1000),
+        type=int,
+    )
+    args, unrecognized_args = parser.parse_known_args()
+
+    return args
+
+
+def get_collections():
+    """Get list of collections to process. todo """
+    return ["demo:claimant", "demo:statement", "demo:contract"]
+
+
+def replica_metadata_refresh():
+    """Refresh replica cluster metadata"""
+    os.system("sudo chmod -R a+rwx /var/log/hbase")
+    os.system('echo "refresh_meta" | hbase shell')
+
+
+def retry_requests(retries=10, backoff=1, methods=None):
+    if methods is None:
+        methods = ["POST"]
+    retry_strategy = Retry(
+        total=retries,
+        backoff_factor=backoff,
+        status_forcelist=[429, 500, 502, 503, 504],
+        allowed_methods=frozenset(methods),
+    )
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    requests_session = requests.Session()
+    requests_session.mount("https://", adapter)
+    requests_session.mount("http://", adapter)
+    return requests_session
+
+
+def get_plaintext_key(url, kek, cek):
+    """Call DKS to return decrypted datakey."""
+    request = retry_requests(methods=["POST"])
+
+    response = request.post(
+        url,
+        params={"keyId": kek, "correlationId": 0},
+        data=cek,
+        cert=(
+            "/etc/pki/tls/certs/private_key.crt",
+            "/etc/pki/tls/private/private_key.key",
+        ),
+        verify="/etc/pki/ca-trust/source/anchors/analytical_ca.pem",
+    )
+    content = response.json()
+    plaintext_key = content["plaintextDataKey"]
+    return plaintext_key
+
+
+def decrypt_ciphertext(ciphertext, key, iv):
+    """Decrypt ciphertext using key & iv."""
+    iv_int = int(binascii.hexlify(base64.b64decode(iv)), 16)
+    counter = Counter.new(AES.block_size * 8, initial_value=iv_int)
+    aes = AES.new(base64.b64decode(key), AES.MODE_CTR, counter=counter)
+    return aes.decrypt(base64.b64decode(ciphertext)).decode("utf8")
+
+
+def decrypt_message(item):
+    """Find and decrypt dbObject, Return full message."""
+    json_item = json.loads(item)
+    iv = json_item["message"]["encryption"]["initialisationVector"]
+    cek = json_item["message"]["encryption"]["encryptedEncryptionKey"]
+    kek = json_item["message"]["encryption"]["keyEncryptionKeyId"]
+    db_obj = json_item["message"]["dbObject"]
+
+    plaintext_key = get_plaintext_key(
+        DKS_DECRYPT_ENDPOINT,
+        kek,
+        cek,
+    )
+    decrypted_obj = decrypt_ciphertext(db_obj, plaintext_key, iv)
+    json_item["message"]["dbObject"] = json.loads(decrypted_obj)
+    del json_item["message"]["encryption"]
+    return json.dumps(json_item)
+
+
+def process_collection(
+    collection, spark, start_time, end_time, s3_root_path, business_date_hour
+):
+    """Extract collection from hbase, decrypt, put in S3."""
+    hbase_table_name = collection
+    hive_table_name = collection.replace(":", "_")
+    hbase_commands = (
+        f"refresh_hfiles '{hbase_table_name}' \n"
+        + f"scan '{hbase_table_name}', {{TIMERANGE => [{start_time}, {end_time}]}}"
+    )
+
+    os.system(
+        f'echo -e "{hbase_commands}" '
+        f"| hbase shell  "
+        f"| hdfs dfs -put -f - hdfs:///{hive_table_name}"
+    )
+
+    def filter_rows(x):
+        return x.find("column=") > -1
+
+    def process_record(x):
+        y = [str.strip(i) for i in re.split(r" *column=|, *timestamp=|, *value=", x)]
+        record_id = y[0]
+        timestamp = y[2]
+        record = decrypt_message(y[3])
+        return Row(id=record_id, timestamp=timestamp, record=record)
+
+    rdd = (
+        spark.sparkContext.textFile(f"hdfs:///{hive_table_name}")
+        .filter(filter_rows)
+        .map(process_record)
+    )
+
+    s3_collection_dir = s3_root_path + hive_table_name + "/"
+    s3_output_dir = s3_collection_dir + business_date_hour + "/"
+    rdd.saveAsTextFile(s3_output_dir)
+    return hive_table_name, s3_collection_dir
+
+
+def create_hive_table(collection_tuple):
+    table_name = collection_tuple[0]
+    s3_path = collection_tuple[1]
+    hive_statements = [
+        f"drop table if exists {table_name}; ",
+        f"create external table if not exists {table_name}(val string) stored as "
+        f'textfile location "{s3_path}"; ',
+    ]
+
+    for sql in hive_statements:
+        spark.sql(sql)
+
+
+def main(spark, collections, start_time, end_time, s3_root_path, business_date_hour):
+    replica_metadata_refresh()
+    try:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            all_collections = executor.map(
+                process_collection,
+                collections,
+                itertools.repeat(spark),
+                itertools.repeat(start_time),
+                itertools.repeat(end_time),
+                itertools.repeat(s3_root_path),
+                itertools.repeat(business_date_hour),
+            )
+    except Exception as e:
+        _logger.error(e)
+        raise e
+
+    # create HIVE tables
+    # try:
+    #     with concurrent.futures.ThreadPoolExecutor() as executor:
+    #         executor.map(create_hive_table, list(all_collections))
+    # except Exception as e:
+    #     _logger.error(e)
+    #     raise e
+
+
+if __name__ == "__main__":
+    args = get_parameters()
+    start_time = args.start_time
+    end_time = args.end_time
+    collections = get_collections()
+    spark = SparkSession.builder.enableHiveSupport().getOrCreate()
+
+    business_date_hour = datetime.datetime.fromtimestamp(end_time / 1000.0).strftime(
+        "%Y%m%d-%H%M"
+    )
+    s3_root_path = f"s3://{INCREMENTAL_OUTPUT_BUCKET}/{INCREMENTAL_OUTPUT_PREFIX}"
+    perf_start = time.perf_counter()
+    main(spark, collections, start_time, end_time, s3_root_path, business_date_hour)
+
+    perf_end = time.perf_counter()
+    total_time = round(perf_end - perf_start)
+    _logger.info(f"time taken to process collections: {total_time}")
