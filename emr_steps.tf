a3a6d9a73a10bec7191670eda873ba5573ce10a7
Toby Walls <60394163+tobywalls@users.noreply.github.com>
2021-04-30 16:07:46 +0100

    fix reference to renamed file

--- emr_steps.tf
+++ emr_steps.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket_object" "adg_incremental_spark_step" {
   bucket = data.terraform_remote_state.common.outputs.config_bucket["id"]
-  key    = "${local.replica_emr_step_scripts_s3_prefix}/spark-job.py"
-  content = templatefile("files/emr/spark-job.py",
+  key    = "${local.replica_emr_step_scripts_s3_prefix}/spark_job.py"
+  content = templatefile("files/emr/spark_job.py",
   {
     dks_decrypt_endpoint = data.terraform_remote_state.crypto.outputs.dks_endpoint[local.environment]
     log_path = "/var/log/adg_incremental_step.log"
