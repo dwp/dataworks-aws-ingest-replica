1ca6d6b1b17ffb38a1a002f32c599e2f1d99d351
suhas.jadhav <onlysunny25@gmail.com>
2021-05-21 16:27:30 +0100

    creating cloudwatch rule

---
+++ files/lambda/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import datetime
+import json
+import logging
+import boto3
+import botocore
+
+def main():
+    print("Hello  world")
+
+def handler(event, context):
+    main()
