1f1e492157e7aeec2c4d2fb65c605df2dd13750c
suhas.jadhav <onlysunny25@gmail.com>
2021-05-26 13:24:13 +0100

    addig code

--- files/lambda/index.py
+++ files/lambda/index.py
@@ -6,10 +6,13 @@ import boto3
 import botocore
 import os
 
-sns_client = boto3.client('sns')
+sns_client = boto3.client("sns")
+db_client = boto3.client("dynamodb")
 
 def main():
     print(f"environment variable: {os.environ['TABLE_NAME']}")
+    rs = db_client.execute_statement(Statement= "select status, id from myTest where status='started' order by id desc")
+    print(rs)
 
 def handler(event, context):
     main()
