72728ab4c1ac57a5465f3a3cb4d0072988c25b4a
suhas.jadhav <onlysunny25@gmail.com>
2021-05-26 14:59:34 +0100

    adding permissions

--- files/lambda/index.py
+++ files/lambda/index.py
@@ -4,11 +4,12 @@ import botocore
 import os
 
 sns_client = boto3.client("sns")
-db_client = boto3.client("dynamodb")
+db_client = boto3.resource("dynamodb")
+# paginator = db_client.get_paginator('list_objects')
 
 def main():
     table_name = os.environ['TABLE_NAME']
-    select = f"select job_status, job_start_time from {table_name} where status='started' order by job_start_time desc;"
+    select = f"select JOB_STATUS, JOB_START_TIME from {table_name} where JOB_STATUS='started' order by JOB_START_TIME desc;"
     rs = db_client.execute_statement(Statement=select)
     print(rs)
 
