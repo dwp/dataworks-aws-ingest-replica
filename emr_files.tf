ae4275c65c90c0f58952b34a5687670a4db6c74b
tobywalls <60394163+tobywalls@users.noreply.github.com>
2021-06-02 14:46:55 +0100

    Change default output location (#63)
    * Output to 'published' bucket
    
    * fmt

--- emr_files.tf
+++ emr_files.tf
@@ -79,7 +79,7 @@ resource "aws_s3_bucket_object" "steps_yaml" {
   key    = "${local.replica_emr_configuration_files_s3_prefix}/steps.yaml"
   content = templatefile("files/emr-config/steps.yaml.tpl",
     {
-      s3_config_bucket = data.terraform_remote_state.common.outputs.config_bucket["id"]
+      s3_config_bucket        = data.terraform_remote_state.common.outputs.config_bucket["id"]
       download_scripts_sh_key = aws_s3_bucket_object.download_scripts.key
 
       pyspark_action_on_failure = "TERMINATE_CLUSTER"
@@ -96,9 +96,8 @@ resource "aws_s3_bucket_object" "generate_dataset_from_hbase" {
     {
       dks_decrypt_endpoint = data.terraform_remote_state.crypto.outputs.dks_endpoint[local.environment]
       log_path             = "/var/log/adg_incremental_step.log"
-      # todo - temporary output location
-      incremental_output_bucket = data.terraform_remote_state.ingest.outputs.s3_buckets["input_bucket"]
-      incremental_output_prefix = "business-data/intra-day/"
+      incremental_output_bucket = data.terraform_remote_state.common.outputs.published_bucket["id"]
+      incremental_output_prefix = "intra-day/"
   })
 
   tags = { Name = "emr-step-generate-dataset-from-hbase" }
