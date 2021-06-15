08c3cf0ee592b64956f3174bc981c62513e3ad66
Toby Walls <60394163+tobywalls@users.noreply.github.com>
2021-06-15 17:42:30 +0100

    Rewritten lambda to track job status

--- files/lambda/index.py
+++ files/lambda/index.py
@@ -1,92 +1,154 @@
+import json
 import logging
-import boto3
-import time
 import os
-import sys
-from botocore.exceptions import ClientError
-from boto3.dynamodb.conditions import Key
+import time
+from uuid import uuid4
 
-sns_client = boto3.client("sns")
-db_client = boto3.resource('dynamodb')
-paginator = db_client.meta.client.get_paginator('query')
+import boto3
+from boto3.dynamodb.conditions import Attr, Key
+
+_logger = logging.getLogger()
+_logger.setLevel(logging.INFO)
+
+# Lambda environment vars
+JOB_STATUS_TABLE = os.environ["job_status_table_name"]
+LAUNCH_SNS_TOPIC_ARN = os.environ["sns_topic_arn"]
+EMR_CONFIG_BUCKET = os.environ["emr_config_bucket"]
+EMR_CONFIG_PREFIX = os.environ["emr_config_folder"]
+
+# Job Statuses & values stored in DynamoDB
+TRIGGERED = "LAMBDA_TRIGGERED"  # this lambda was triggered
+WAITING = "WAITING"  # this lambda is waiting up to 10 minutes for another cluster
+DEFERRED = "DEFERRED"  # this lambda timed out waiting, did not launch emr
+LAUNCHED = "LAUNCHED"  # this lambda posted to SNS topic to launch EMR cluster
+PROCESSING = "PROCESSING"  # emr cluster has started processing data
+COMPLETED = "COMPLETED"  # emr cluster processed the data successfully
+EMR_FAILED = "EMR_FAILED"  # emr cluster couldn't process data successfully
+LAMBDA_FAILED = "LAMBDA_FAILED"  # lambda encountered an error
+
+# this lambda will not launch emr if another job is in one of these states
+ACTIVE_STATES = [TRIGGERED, WAITING, LAUNCHED, PROCESSING, EMR_FAILED, LAMBDA_FAILED]
 
 
-def setup_logging(log_level, log_path=None):
-    logger = logging.getLogger()
-    for old_handler in logger.handlers:
-        logger.removeHandler(old_handler)
+class PollingTimeoutError(TimeoutError):
+    pass
 
-    if log_path is None:
-        handler = logging.StreamHandler(sys.stdout)
-    else:
-        handler = logging.FileHandler(log_path)
 
-    json_format = (
-        "{ 'timestamp': '%(asctime)s', 'log_level': '%(levelname)s', 'message': "
-        "'%(message)s' } "
+def update_db_item(table, correlation_id: str, triggered_time: int, values: dict):
+    updates = {key: {"Value": value} for key, value in values.items()}
+
+    table.update_item(
+        Key={
+            "CorrelationId": correlation_id,
+            "TriggeredTime": triggered_time,
+        },
+        AttributeUpdates=updates,
     )
-    handler.setFormatter(logging.Formatter(json_format))
-    logger.addHandler(handler)
-    new_level = logging.getLevelName(log_level.upper())
-    logger.setLevel(new_level)
-
-    return logger
 
 
-def get_dynamodb(table_name):
-    try:
-        page_iterator = paginator.paginate(TableName=table_name,
-                                           KeyConditionExpression=Key('JOB_STATUS').eq('started'),
-                                           ScanIndexForward=False,
-                                           PaginationConfig={'MaxItems': 1})
-    except ClientError as error:
-        _logger.info(error)
-        sys.exit(1)
-    else:
-        return page_iterator
-
-
-def publish_sns(topic, msg, subject="HBASE incremental refresh job"):
-    try:
-        rs = sns_client.publish(TopicArn = topic,
-                                Message = msg,
-                                Subject = subject,
-                                )
-    except ClientError as error:
-        _logger.info(error)
-        sys.exit(2)
-    else:
-        return rs
-
-
-def main():
-    _logger = setup_logging(
-        log_level="INFO"
+def check_for_running_jobs(table, correlation_id):
+    _logger.debug("Checking for running jobs")
+    results = table.scan(
+        IndexName="StatusIndex",
+        FilterExpression=Attr("JobStatus").is_in(ACTIVE_STATES)
+        & Attr("CorrelationId").ne(correlation_id),
     )
-    table_name = os.environ["table_name"]
-    s3_bucket = os.environ["bucket"]
-    s3_folder = os.environ["folder"]
-    topic = os.environ["topic"]
-    msg = f"""{{
-    "s3_overrides": {{
-        "emr_launcher_config_s3_bucket": {s3_bucket},
-        "emr_launcher_config_s3_folder": {s3_folder}
-    }}
-}}"""
-    counter = 0
+    return results["Items"]
 
-    while counter < 3:
-        status = get_dynamodb(table_name)
-        for page in status:
-            if not page.get("Items", None):
-                _logger.info(f"Launching EMR cluster")
-                rs = publish_sns(topic, msg)
-                _logger.info(rs)
-                counter = 3
-            else:
-                _logger.info(f"Previous job is still running will check the status in another 5 minutes {page}")
-                time.sleep(300)
-                counter += 1
+
+def poll_previous_jobs(correlation_id, triggered_time, table, timeout=300):
+    update_db_item(table, correlation_id, triggered_time, {"JobStatus": WAITING})
+    start_time = time.time()
+
+    while check_for_running_jobs(table, correlation_id):
+        _logger.info(
+            f"Waited {round(time.time() - start_time)}/{timeout}"
+            f" seconds for previous jobs to complete"
+        )
+        if time.time() >= (start_time + timeout):
+            update_db_item(
+                table, correlation_id, triggered_time, {"JobStatus": DEFERRED}
+            )
+            raise PollingTimeoutError(
+                f"Polling timeout ({timeout}s), job(s) still in progress"
+            )
+        time.sleep(5)
+    return True
+
+
+def get_job_context(sns_client, table):
+    # Get latest timestamp for data processed
+    _logger.debug("getting timestamps from previous jobs")
+    response = table.query(
+        IndexName="StatusIndex",
+        KeyConditionExpression=Key("JobStatus").eq("COMPLETED"),
+        ProjectionExpression="TriggeredTime, ProcessedDataStart, ProcessedDataEnd",
+        ScanIndexForward=False,
+    )
+
+    filtered_records = [
+        record for record in response["Items"] if record.get("ProcessedDataEnd")
+    ]
+    end_time = filtered_records[0].get("ProcessedDataEnd") if filtered_records else 0
+    return end_time
+
+
+def launch_cluster(
+    correlation_id: str, triggered_time, sns_client, job_table, topic_arn: str
+):
+    previous_end_time = get_job_context(sns_client, job_table)
+    buffer = 1000 * 60 * 5  # 5 minutes in milliseconds
+    new_end_time = round(time.time() * 1000) - buffer
+
+    cluster_config = json.dumps(
+        {
+            "s3_overrides": {
+                "emr_launcher_config_s3_bucket": EMR_CONFIG_BUCKET,
+                "emr_launcher_config_s3_folder": EMR_CONFIG_PREFIX,
+            },
+            "additional_step_args": {
+                "spark-submit": [
+                    "--start_time",
+                    int(previous_end_time),
+                    "--end_time",
+                    int(new_end_time),
+                    "--correlation_id",
+                    str(correlation_id),
+                ]
+            },
+        }
+    )
+    _logger.info("Launching emr cluster")
+    _logger.debug({"Cluster Config": cluster_config})
+    _ = sns_client.publish(
+        TopicArn=topic_arn,
+        Message=cluster_config,
+        Subject="Launch ingest-replica emr cluster",
+    )
+    update_db_item(job_table, correlation_id, triggered_time, {"JobStatus": LAUNCHED})
+
 
 def handler(event, context):
-    main()
+    correlation_id = str(uuid4())
+    triggered_time = round(time.time() * 1000)
+
+    sns_client = boto3.client("sns")
+    dynamodb = boto3.resource("dynamodb")
+    job_table = dynamodb.Table(JOB_STATUS_TABLE)
+
+    update_db_item(job_table, correlation_id, triggered_time, {"JobStatus": TRIGGERED})
+
+    try:
+        poll_previous_jobs(correlation_id, triggered_time, job_table)
+        launch_cluster(
+            correlation_id, triggered_time, sns_client, job_table, LAUNCH_SNS_TOPIC_ARN
+        )
+    except PollingTimeoutError:
+        # Dynamodb already updated with status
+        raise
+    except Exception:
+        # Update Dynamodb with failure status
+        update_db_item(
+            job_table, correlation_id, triggered_time, {"JobStatus": LAMBDA_FAILED}
+        )
+        raise
