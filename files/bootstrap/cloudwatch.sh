3bf628f420f7de0e98722cb201a57535fb580a1d
Toby Walls <60394163+tobywalls@users.noreply.github.com>
2021-06-22 09:21:00 +0100

    SC exception

--- files/bootstrap/cloudwatch.sh
+++ files/bootstrap/cloudwatch.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 set -Eeuo pipefail
+# shellcheck disable=SC2034
+# shellcheck disable=SC1083
 (
-cwa_metrics_collection_interval="${cwa_metrics_collection_interval}"
-cwa_namespace="${cwa_namespace}"
-cwa_log_group_name="${cwa_log_group_name}"
-cwa_bootstrap_loggrp_name="${cwa_bootstrap_loggrp_name}"
-cwa_steps_loggrp_name="${cwa_steps_loggrp_name}"
-cwa_yarnspark_loggrp_name="${cwa_yarnspark_loggrp_name}"
-cwa_tests_loggrp_name="${cwa_tests_loggrp_name}"
-step_log_path="${step_log_path}"
+collection_interval="${cwa_metrics_collection_interval}"
+namespace="${cwa_namespace}"
+lg_name="${cwa_log_group_name}"
+lg_name_bootstrap="${cwa_bootstrap_loggrp_name}"
+lg_name_steps="${cwa_steps_loggrp_name}"
+lg_name_yarnspark="${cwa_yarnspark_loggrp_name}"
+lg_name_tests="${cwa_tests_loggrp_name}"
+lg_path_steps="${step_log_path}"
 
 export AWS_DEFAULT_REGION="${aws_default_region}"
 
@@ -17,7 +19,7 @@ mkdir -p /opt/aws/amazon-cloudwatch-agent/etc
 cat > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json <<CWAGENTCONFIG
 {
   "agent": {
-    "metrics_collection_interval": $${cwa_metrics_collection_interval},
+    "metrics_collection_interval": $${collection_interval},
     "logfile": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log"
   },
   "logs": {
@@ -26,98 +28,98 @@ cat > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json <<CWAGEN
         "collect_list": [
           {
             "file_path": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log",
-            "log_group_name": "$${cwa_log_group_name}",
+            "log_group_name": "$${lg_name}",
             "log_stream_name": "{instance_id}-amazon-cloudwatch-agent.log",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/messages",
-            "log_group_name": "$${cwa_log_group_name}",
+            "log_group_name": "$${lg_name}",
             "log_stream_name": "{instance_id}-messages",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/secure",
-            "log_group_name": "$${cwa_log_group_name}",
+            "log_group_name": "$${lg_name}",
             "log_stream_name": "{instance_id}-secure",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/cloud-init-output.log",
-            "log_group_name": "$${cwa_log_group_name}",
+            "log_group_name": "$${lg_name}",
             "log_stream_name": "{instance_id}-cloud-init-output.log",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/emr-bootstrap/installer.log",
-            "log_group_name": "$${cwa_bootstrap_loggrp_name}",
+            "log_group_name": "$${lg_name_bootstrap}",
             "log_stream_name": "{instance_id}-installer.log",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/emr-bootstrap/install-pycrypto.log",
-            "log_group_name": "$${cwa_bootstrap_loggrp_name}",
+            "log_group_name": "$${lg_name_bootstrap}",
             "log_stream_name": "{instance_id}-install-pycrypto.log",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/emr-bootstrap/install-boto3.log",
-            "log_group_name": "$${cwa_bootstrap_loggrp_name}",
+            "log_group_name": "$${lg_name_bootstrap}",
             "log_stream_name": "{instance_id}-install-boto3.log",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/emr-bootstrap/install-requests.log",
-            "log_group_name": "$${cwa_bootstrap_loggrp_name}",
+            "log_group_name": "$${lg_name_bootstrap}",
             "log_stream_name": "{instance_id}-install-requests.log",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/emr-bootstrap/download_scripts.log",
-            "log_group_name": "$${cwa_bootstrap_loggrp_name}",
+            "log_group_name": "$${lg_name_bootstrap}",
             "log_stream_name": "{instance_id}-download_scripts.log",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/emr-bootstrap/acm-cert-retriever.log",
-            "log_group_name": "$${cwa_bootstrap_loggrp_name}",
+            "log_group_name": "$${lg_name_bootstrap}",
             "log_stream_name": "{instance_id}-acm-cert-retriever.log",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/hadoop-yarn/containers/application_*/container_*/stdout**",
-            "log_group_name": "$${cwa_yarnspark_loggrp_name}",
+            "log_group_name": "$${lg_name_yarnspark}",
             "log_stream_name": "{instance_id}-spark-stdout.log",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/hadoop-yarn/containers/application_*/container_*/stderr**",
-            "log_group_name": "$${cwa_yarnspark_loggrp_name}",
+            "log_group_name": "$${lg_name_yarnspark}",
             "log_stream_name": "{instance_id}-spark-stderror.log",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/hadoop-yarn/yarn-yarn-nodemanager**.log",
-            "log_group_name": "$${cwa_yarnspark_loggrp_name}",
+            "log_group_name": "$${lg_name_yarnspark}",
             "log_stream_name": "{instance_id}-yarn_nodemanager.log",
             "timezone": "UTC"
           },
           {
-            "file_path": "$${step_log_path}",
-            "log_group_name": "$${cwa_steps_loggrp_name}",
+            "file_path": "$${lg_path_steps}",
+            "log_group_name": "$${lg_name_steps}",
             "log_stream_name": "{instance_id}-steps.log",
             "timezone": "UTC"
           },
           {
             "file_path": "/var/log/tests.log",
-            "log_group_name": "$${cwa_tests_loggrp_name}",
+            "log_group_name": "$${lg_name_tests}",
             "log_stream_name": "{instance_id}-tests.log",
             "timezone": "UTC"
           },
         ]
       }
     },
-    "log_stream_name": "$${cwa_namespace}",
+    "log_stream_name": "$${namespace}",
     "force_flush_interval" : 15
   }
 }
