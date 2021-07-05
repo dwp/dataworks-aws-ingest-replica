14ec44203a30a00b1cc8795af012503923268e8e
Toby Walls <60394163+tobywalls@users.noreply.github.com>
2021-07-05 16:08:20 +0100

    Track collections individually within pyspark code - get latest processed timestamp from dynamodb, and put updates

--- files/lambda/index.py
+++ files/lambda/index.py
@@ -3,9 +3,11 @@ import logging
 import os
 import time
 from uuid import uuid4
-
+import base64
+import ast
 import boto3
 from boto3.dynamodb.conditions import Attr, Key
+from enum import Enum
 
 _logger = logging.getLogger()
 _logger.setLevel(logging.INFO)
@@ -15,14 +17,16 @@ JOB_STATUS_TABLE = os.environ["job_status_table_name"]
 LAUNCH_SNS_TOPIC_ARN = os.environ["sns_topic_arn"]
 EMR_CONFIG_BUCKET = os.environ["emr_config_bucket"]
 EMR_CONFIG_PREFIX = os.environ["emr_config_folder"]
+COLLECTIONS_SECRET_NAME = os.environ["collections_secret_name"]
+
 
 # Job Statuses & values stored in DynamoDB
 TRIGGERED = "LAMBDA_TRIGGERED"  # this lambda was triggered
 WAITING = "WAITING"  # this lambda is waiting up to 10 minutes for another cluster
 DEFERRED = "DEFERRED"  # this lambda timed out waiting, did not launch emr
-LAUNCHED = "LAUNCHED"  # this lambda posted to SNS topic to launch EMR cluster
-PROCESSING = "PROCESSING"  # emr cluster has started processing data
-COMPLETED = "COMPLETED"  # emr cluster processed the data successfully
+LAUNCHED = "EMR_LAUNCHED"  # this lambda posted to SNS topic to launch EMR cluster
+PROCESSING = "EMR_PROCESSING"  # emr cluster has started processing data
+COMPLETED = "EMR_COMPLETED"  # emr cluster processed the data successfully
 EMR_FAILED = "EMR_FAILED"  # emr cluster couldn't process data successfully
 LAMBDA_FAILED = "LAMBDA_FAILED"  # lambda encountered an error
 
@@ -34,73 +38,87 @@ class PollingTimeoutError(TimeoutError):
     pass
 
 
-def update_db_item(table, correlation_id: str, triggered_time: int, values: dict):
+def get_collections_list_from_aws(secrets_client, collections_secret_name):
+    """Parse collections returned by AWS Secrets Manager"""
+    return [
+        f"{j['db']}:{j['table']}"
+        for j in retrieve_secrets(secrets_client, collections_secret_name)[
+            "collections_all"
+        ].values()
+    ]
+
+
+def retrieve_secrets(secrets_client, secret_name):
+    """Get b64 encoded secrets from AWS Secrets Manager"""
+    response = secrets_client.get_secret_value(SecretId=secret_name)
+    response_binary = response["SecretString"]
+    response_decoded = base64.b64decode(response_binary).decode("utf-8")
+    response_dict = ast.literal_eval(response_decoded)
+    return response_dict
+
+
+def update_db_items(table, collections, correlation_id: str, values: dict):
     updates = {key: {"Value": value} for key, value in values.items()}
+    for collection in collections:
+        table.update_item(
+            Key={
+                "CorrelationId": correlation_id,
+                "Collection": collection,
+            },
+            AttributeUpdates=updates,
+        )
 
-    table.update_item(
-        Key={
-            "CorrelationId": correlation_id,
-            "TriggeredTime": triggered_time,
-        },
-        AttributeUpdates=updates,
-    )
 
-
-def check_for_running_jobs(table, correlation_id):
+def check_for_running_jobs(table, collections, correlation_id):
     _logger.debug("Checking for running jobs")
     results = table.scan(
-        IndexName="StatusIndex",
         FilterExpression=Attr("JobStatus").is_in(ACTIVE_STATES)
-        & Attr("CorrelationId").ne(correlation_id),
+        & Attr("CorrelationId").ne(correlation_id)
+        & Attr("Collection").is_in(collections),
     )
     return results["Items"]
 
 
-def poll_previous_jobs(correlation_id, triggered_time, table, timeout=300):
-    update_db_item(table, correlation_id, triggered_time, {"JobStatus": WAITING})
+def poll_previous_jobs(correlation_id, collections, table, timeout=300):
+    update_db_items(table, collections, correlation_id, {"JobStatus": WAITING})
     start_time = time.time()
 
-    while check_for_running_jobs(table, correlation_id):
+    running_jobs = check_for_running_jobs(table, collections, correlation_id)
+    while running_jobs:
         _logger.info(
             f"Waited {round(time.time() - start_time)}/{timeout}"
-            f" seconds for previous jobs to complete"
+            f" seconds for previous collections to complete:"
+        )
+        _logger.info("\n".join(
+                [
+                    str({"topic": row["Collection"], "JobStatus": row["JobStatus"]})
+                    for row in running_jobs
+                ]
+            )
         )
         if time.time() >= (start_time + timeout):
-            update_db_item(
-                table, correlation_id, triggered_time, {"JobStatus": DEFERRED}
-            )
+            update_db_items(table, collections, correlation_id, {"JobStatus": DEFERRED})
             raise PollingTimeoutError(
                 f"Polling timeout ({timeout}s), job(s) still in progress"
             )
         time.sleep(5)
+        running_jobs = check_for_running_jobs(table, collections, correlation_id)
     return True
 
 
-def get_job_context(sns_client, table):
-    # Get latest timestamp for data processed
-    _logger.debug("getting timestamps from previous jobs")
-    response = table.query(
-        IndexName="StatusIndex",
-        KeyConditionExpression=Key("JobStatus").eq("COMPLETED"),
-        ProjectionExpression="TriggeredTime, ProcessedDataStart, ProcessedDataEnd",
-        ScanIndexForward=False,
-    )
-
-    filtered_records = [
-        record for record in response["Items"] if record.get("ProcessedDataEnd")
-    ]
-    end_time = filtered_records[0].get("ProcessedDataEnd") if filtered_records else 0
-    return end_time
-
-
 def launch_cluster(
-    correlation_id: str, triggered_time, sns_client, job_table, topic_arn: str
+    correlation_id: str,
+    triggered_time: int,
+    collections,
+    sns_client,
+    job_table,
+    topic_arn: str,
 ):
-    previous_end_time = get_job_context(sns_client, job_table)
-    buffer = 1000 * 60 * 5  # 5 minutes in milliseconds
-    new_end_time = round(time.time() * 1000) - buffer
+    # Cluster takes 10~15m to provision, this provides adequate time for the pipeline
+    #   to ingest data up to the current timestamp into hbase.
+    new_end_time = int(time.time() * 1000)
 
-    cluster_config = json.dumps(
+    cluster_overrides = json.dumps(
         {
             "s3_overrides": {
                 "emr_launcher_config_s3_bucket": EMR_CONFIG_BUCKET,
@@ -108,27 +126,28 @@ def launch_cluster(
             },
             "additional_step_args": {
                 "spark-submit": [
-                    "--start_time",
-                    str(previous_end_time),
-                    "--end_time",
-                    str(new_end_time),
                     "--correlation_id",
                     str(correlation_id),
                     "--triggered_time",
                     str(triggered_time),
-                    "--tracked"
+                    "--end_time",
+                    str(new_end_time),
+                    "--tracked",
+                    "--collections",
+                    " ".join(collections),
                 ]
             },
         }
     )
     _logger.info("Launching emr cluster")
-    _logger.debug({"Cluster Config": cluster_config})
+    _logger.info("Collections: " + " ".join(collections))
+    _logger.debug({"Cluster Overrides": cluster_overrides})
     _ = sns_client.publish(
         TopicArn=topic_arn,
-        Message=cluster_config,
+        Message=cluster_overrides,
         Subject="Launch ingest-replica emr cluster",
     )
-    update_db_item(job_table, correlation_id, triggered_time, {"JobStatus": LAUNCHED})
+    update_db_items(job_table, collections, correlation_id, {"JobStatus": LAUNCHED})
 
 
 def handler(event, context):
@@ -138,20 +157,34 @@ def handler(event, context):
     sns_client = boto3.client("sns")
     dynamodb = boto3.resource("dynamodb")
     job_table = dynamodb.Table(JOB_STATUS_TABLE)
+    secrets_client = boto3.session.Session().client(service_name="secretsmanager")
 
-    update_db_item(job_table, correlation_id, triggered_time, {"JobStatus": TRIGGERED})
+    collections = get_collections_list_from_aws(secrets_client, COLLECTIONS_SECRET_NAME)
+    update_db_items(
+        job_table,
+        collections,
+        correlation_id,
+        {"JobStatus": TRIGGERED, "TriggeredTime": triggered_time},
+    )
 
     try:
-        poll_previous_jobs(correlation_id, triggered_time, job_table)
-        launch_cluster(
-            correlation_id, triggered_time, sns_client, job_table, LAUNCH_SNS_TOPIC_ARN
+        poll_previous_jobs(
+            correlation_id=correlation_id, collections=collections, table=job_table
         )
-    except PollingTimeoutError:
+        launch_cluster(
+            correlation_id=correlation_id,
+            triggered_time=triggered_time,
+            collections=collections,
+            sns_client=sns_client,
+            job_table=job_table,
+            topic_arn=LAUNCH_SNS_TOPIC_ARN,
+        )
+    except PollingTimeoutError as e:
         # Dynamodb already updated with status
         raise
     except Exception:
         # Update Dynamodb with failure status
-        update_db_item(
-            job_table, correlation_id, triggered_time, {"JobStatus": LAMBDA_FAILED}
+        update_db_items(
+            job_table, collections, correlation_id, {"JobStatus": LAMBDA_FAILED}
         )
         raise
