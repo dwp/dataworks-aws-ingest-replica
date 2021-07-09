41642bab03271c62746e4e3357f62664dd4232a3
Toby Walls <60394163+tobywalls@users.noreply.github.com>
2021-07-09 12:17:51 +0100

    fmt

--- emr_files.tf
+++ emr_files.tf
@@ -111,8 +111,8 @@ resource "aws_s3_bucket_object" "generate_dataset_from_hbase" {
 }
 
 resource "aws_s3_bucket_object" "generate_dataset_from_adg" {
-  bucket = data.terraform_remote_state.common.outputs.config_bucket["id"]
-  key    = "${local.replica_emr_step_scripts_s3_prefix}/generate_dataset_from_adg.py"
+  bucket  = data.terraform_remote_state.common.outputs.config_bucket["id"]
+  key     = "${local.replica_emr_step_scripts_s3_prefix}/generate_dataset_from_adg.py"
   content = file("files/steps/generate_dataset_from_adg.py")
 
   tags = { Name = "emr-step-generate-dataset-from-adg" }
