23ef62a3ecc07eb266622b17b3f3e82996c30beb
tobywalls <60394163+tobywalls@users.noreply.github.com>
2021-08-17 09:31:22 +0100

    rename secret (#111)

--- local.tf
+++ local.tf
@@ -1,5 +1,5 @@
 locals {
-  collections_secret_name = "/ingest-replica/collections"
+  collections_secret_name = "/intraday/collections"
 
   persistence_tag_value = {
     development = "Ignore" // "mon-fri,08:00-18:00"
